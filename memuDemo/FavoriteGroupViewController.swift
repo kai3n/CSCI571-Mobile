33a367090e8f3c4be8837a8706bf42356485a5c9
Mingun Pak <mingunpa@usc.edu>
2017-04-10 23:44:13 -0700

    Initial Commit

---
+++ memuDemo/FavoriteGroupViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FavoriteGroupViewController.swift
+//  memuDemo
+//
+//  Created by kaien on 4/10/17.
+//  Copyright © 2017 Parth Changela. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteGroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ memuDemo/HomeScreenViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  memuDemo
+//
+//  Created by Parth Changela on 09/10/16.
+//  Copyright © 2016 Parth Changela. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+
+    @IBOutlet weak var btnMenuButton: UIBarButtonItem!
+    @IBAction func moveToTabBar(_ sender: Any) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let resultViewController = storyBoard.instantiateViewController(withIdentifier: "SWRevealViewController2")
+        self.present(resultViewController, animated:false, completion:nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        if revealViewController() != nil {
+            //            revealViewController().rearViewRevealWidth = 62
+            btnMenuButton.target = revealViewController()
+            btnMenuButton.action = "revealToggle:"
+            
+//            revealViewController().rightViewRevealWidth = 150
+//            extraButton.target = revealViewController()
+//            extraButton.action = "rightRevealToggle:"
+            
+        }
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
